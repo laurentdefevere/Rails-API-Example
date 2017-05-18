fa2af609dfa951dd5d15f69018e22e94be1eb196
Robbie Smith <rsmith@peaksware.com>
2017-05-18 11:10:54 -0600

    Updates routs and link view to use link

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get '/login', to: 'login#new', as: 'login'
+  root 'link#new'
+  get '/link', to: 'link#new', as: 'login'
   resources :sessions, only: [:new, :create]
 end
