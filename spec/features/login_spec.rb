7748c814f7acd89933bbb2ce35c76439fa209850
Robbie Smith <rsmith@peaksware.com>
2017-05-18 10:41:54 -0600

    adds faraday and first correct redirect

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -6,12 +6,16 @@ feature 'oauth' do
     before { visit '/login' }
 
     it 'when the user logs in' do
-
       expect(page).to have_current_path('/login')
     end
 
     it 'should have a login button' do
       expect(page).to have_selector(:link_or_button, 'Login')
+    end
+
+    it 'authenticates the user' do
+      click_on 'Login'
+      expect(page).to have_current_path('/')
     end
   end
 end
