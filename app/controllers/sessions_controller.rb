39efb55cf4c301706b7c77bc88deff55f7eb6eda
Robbie Smith <rsmith@peaksware.com>
2017-05-18 11:08:21 -0600

    Rename login to link

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
   def new
-    redirect_to "https://oauth.sandbox.trainingpeaks.com/OAuth/Authorize/?response_type=code&client_id=quantisync&scope=metrics:write"
+    redirect_to "https://oauth.sandbox.trainingpeaks.com/OAuth/Authorize/?response_type=code&client_id=quantisync&scope=metrics:write&redirect_uri=https://localhost:3000"
   end
 end
