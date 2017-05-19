98a0f3d6aae14456e1728b357e2b118843260d10
Robbie Smith <rsmith@peaksware.com>
2017-05-19 16:24:59 -0600

    Cleans up ui

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -4,23 +4,24 @@ class PostController < ApplicationController
   def new; end
 
   def create
-    conn = Faraday.new('https://api.sandbox.trainingpeaks.com') do |faraday|
+    conn = Faraday.new("https://api.sandbox.trainingpeaks.com") do |faraday|
       faraday.request :url_encoded
       faraday.adapter Faraday.default_adapter
     end
 
     response = conn.post do |req|
-      req.url '/v1/metrics'
-      req.headers['Content-Type'] = 'application/json'
-      req.headers['Authorization'] = "Bearer #{token.access_token}"
+      req.url "/v1/metrics"
+      req.headers["Content-Type"] = "application/json"
+      req.headers["Authorization"] = "Bearer #{token.access_token}"
       req.params = {
-        'scope': 'metrics:write',
-        'DateTime': DateTime.new.utc,
-        'UploadClient': params[:upload_client],
-        'Appetite': params[:appetite]
+        "scope": "metrics:write",
+        "DateTime": DateTime.now.utc,
+        "UploadClient": params[:upload_client],
+        "Appetite": params[:appetite]
       }
       req.body = req.params.to_json
     end
+    redirect_to new_post_path
   end
 
   private
